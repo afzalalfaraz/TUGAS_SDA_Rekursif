6b0c036cce15267e485b2165bdf255758ce9fb64
afzalalfaraz <99129704+afzalalfaraz@users.noreply.github.com>
2022-06-05 22:24:47 +0700

    Add files via upload

---
+++ segitigarekursifafzal.cpp
@@ -0,0 +1,29 @@
+//Afzal Alfaraz
+//G1A021098
+
+#include <iostream>
+using namespace std;
+
+void printn(int jumlah)
+{
+    if (jumlah == 0)
+        return;
+    cout << "* ";
+    printn(jumlah - 1);
+}
+ 
+void segitiga(int n, int i){
+    if (n == 0)
+        return;
+    printn(i);
+    cout << endl;
+    segitiga(n - 1, i + 1);
+}
+int main()
+{
+    int n;
+    printf("masukkan jumlah : ");
+    cin>>n;
+    segitiga(n, 1);
+    return 0;
+}
